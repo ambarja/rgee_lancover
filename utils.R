7ffcdeebd6b9ae0122c4a0d228644f630006f486
Antony Barja <antony.barja8@gmail.com>
2022-02-05 12:13:53 -0500

    Update utils.R

--- utils.R
+++ utils.R
@@ -5,7 +5,8 @@ pca_rgee <- function(x){
   covar  <- arrays$reduceRegion(
     reducer = ee$Reducer$centeredCovariance(),
     geometry = nc,
-    scale = 100
+    scale = 100,
+    maxPixels = 10**19
   )
   
   covarArray = ee$Array(covar$get('array'))
@@ -16,8 +17,9 @@ pca_rgee <- function(x){
   pca = ee$Image(eigenVectors)$matrixMultiply(arrayImage)
   
   sd_img = ee$Image(eigenValues$sqrt())$
-    arrayProject(list(0))$arrayFlatten(list(c("sd1","sd2","sd3","sd4","sd5","sd6")))
-
+    arrayProject(list(0))$
+    arrayFlatten(list(c("sd1","sd2","sd3","sd4","sd5","sd6")))
+  
   pca_raster <- pca$
     arrayProject(list(0))$
     arrayFlatten(list(c("pc1","pc2","pc3","pc4","pc5","pc6")))$
@@ -29,16 +31,59 @@ pca_rgee <- function(x){
 # Standar image
 img_to_stand <- function(x){
   mean <- x$reduceRegion(
-  reducer =  ee$Reducer$mean(),
-  geometry = nc,
-  scale =  30,
-  maxPixels = 10**19
+    reducer =  ee$Reducer$mean(),
+    geometry = nc,
+    scale =  30,
+    maxPixels = 10**19
   )
   values_mean <- mean$values()
   img_mean <- ee$Image$constant(values_mean)
   centered <- x$subtract(img_mean)
   return(centered)
 }  
+
+# Compute eignvectors 
+eingvector_rgee <- function(x){
+  arrays <- x$toArray()
+  covar  <- arrays$reduceRegion(
+    reducer = ee$Reducer$centeredCovariance(),
+    geometry = nc,
+    scale = 30,
+    maxPixels = 10**19
+  )
+  
+  covarArray = ee$Array(covar$get('array'))
+  eigens = covarArray$eigen()
+  eigenValues = eigens$slice(1, 0, 1)
+  eigenVectors = eigens$slice(1, 1)
+  
+  table_eingvalues <- eigenVectors$getInfo() %>%
+    map_dfr(~unlist(.x) %>% t() %>% as.data.frame(),.id = 'eingvector') %>% 
+    rename_at(2:7,~c("nldc","imp" ,"tree","ndvi","tmax","tmin"))
+  
+  return(table_eingvalues)
+}
+
+# Compute eingvalues
+eingvalues_rgee <- function(x){
+  arrays <- x$toArray()
+  covar  <- arrays$reduceRegion(
+    reducer = ee$Reducer$centeredCovariance(),
+    geometry = nc,
+    scale = 30,
+    maxPixels = 10**19
+  )
+  
+  covarArray = ee$Array(covar$get('array'))
+  eigens = covarArray$eigen()
+  eigenValues = eigens$slice(1, 0, 1)
+  eing_values <- eigenValues$getInfo() %>%
+    map_df(as.data.frame,.id = 'Eig') %>% 
+    rename(values =`.x[[i]]`) %>%
+    mutate(Eig = paste0("Eig.",Eig))
+  
+  return(eing_values)
+}
 
 
 # Importance of component 
@@ -74,47 +119,6 @@ impor_pca <- function(x){
   
   return(var_comp)
   
-}
-
-eingvector_rgee <- function(x){
-  arrays <- x$toArray()
-  covar  <- arrays$reduceRegion(
-    reducer = ee$Reducer$centeredCovariance(),
-    geometry = nc,
-    scale = 30,
-    maxPixels = 10**19
-  )
-  
-  covarArray = ee$Array(covar$get('array'))
-  eigens = covarArray$eigen()
-  eigenValues = eigens$slice(1, 0, 1)
-  eigenVectors = eigens$slice(1, 1)
-  
-  table_eingvalues <- eigenVectors$getInfo() %>%
-    map_dfr(~unlist(.x) %>% t() %>% as.data.frame(),.id = 'eingvector') %>% 
-    rename_at(2:7,~c("nldc","imp" ,"tree","ndvi","tmax","tmin"))
-
-  return(table_eingvalues)
-}
-
-eingvalues_rgee <- function(x){
-  arrays <- x$toArray()
-  covar  <- arrays$reduceRegion(
-    reducer = ee$Reducer$centeredCovariance(),
-    geometry = nc,
-    scale = 30,
-    maxPixels = 10**19
-  )
-  
-  covarArray = ee$Array(covar$get('array'))
-  eigens = covarArray$eigen()
-  eigenValues = eigens$slice(1, 0, 1)
-  eing_values <- eigenValues$getInfo() %>%
-    map_df(as.data.frame,.id = 'Eig') %>% 
-    rename(values =`.x[[i]]`) %>%
-    mutate(Eig = paste0("Eig.",Eig))
-  
-  return(eing_values)
 }
 
 # Resample size 
